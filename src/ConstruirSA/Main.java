a23ffaccbc7fc34805214826267c378d926356d3
Nicolas Hollmann <nicolashollmanncod3@gmail.com>
2024-05-10 17:46:51 -0300

    Insert empleados

--- src/ConstruirSA/Main.java
+++ src/ConstruirSA/Main.java
@@ -1,3 +1,12 @@
+//Desde el método main de un nuevo proyecto Java de nombre construirSA:
+//
+//#Cargar el driver MariaDB. ^
+//#Establecer la conexión a la base de datos previamente creada. ^
+//#Insertar 3 empleados.
+//#Insertar 2 herramientas.
+//#Listar todas las herramientas con stock superior a 10.
+//#Dar de baja al primer empleado ingresado a la base de datos.
+
 package ConstruirSA;
 import java.sql.*;
 import javax.swing.JOptionPane;
@@ -6,6 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        
         try {
             Class.forName("org.mariadb.jdbc.Driver");
             Connection conexion = DriverManager.getConnection(
@@ -13,16 +23,43 @@ public class Main {
                     "root",
                     "");
             
-            JOptionPane.showMessageDialog(null, "trabajador agregado");
+//            JOptionPane.showMessageDialog(null, "trabajador agregado");
             
+            
+            //INSERT EMPLEADOS = ESCLAVOS
+            
+//            String sql= "insert into empleado(dni, apellido, nombre, acceso, estado)"
+//                    + " values "
+//                    + "(42242442, 'hollmann', 'nicolas', 3, 1),"
+//                    + "(12436578, 'adam', 'rigg', 2,1),"
+//                    + "(98786354, 'enzo', 'casalegno',1,1);";
+            
+            String sql= "insert into empleado(dni, apellido, nombre, acceso, estado)"
+                    + " values "
+                    + "(42242876, Allendez, 'Alexis', 4, 1);";
+            
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            int filas = ps.executeUpdate();
+            if(filas > 0){
+                JOptionPane.showMessageDialog(null, "Empleados agregado exitosamente");
+                System.out.println(filas);
+            }
+            
+            
+            
+            
+            
+            
+            //MANEJO DE ERRORES CONEXIONES
         } catch (SQLException ex) {
+            ex.printStackTrace();
             
            JOptionPane.showMessageDialog(null, "Error de conexion");
             
         } catch (ClassNotFoundException ex) {
             JOptionPane.showMessageDialog(null, "Error al cargar driver");
         }
-
+        
     }
 
 }
