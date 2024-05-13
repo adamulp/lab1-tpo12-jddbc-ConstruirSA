23e55193ca3f4cf3c4dbe2971592a6171934de78
adamulp <adamgoestoargentina@gmail.com>
2024-05-13 09:40:10 -0300

    Update Main.java
    Agrega puntos enumerados de las consignas, variables propias por ejercicio

--- src/ConstruirSA/Main.java
+++ src/ConstruirSA/Main.java
@@ -17,57 +17,75 @@ public class Main {
 
         
         try {
+            // 1. Cargar el driver MariaDB.
             Class.forName("org.mariadb.jdbc.Driver");
+            
+            // 2. Establecer la conexión a la base de datos previamente creada.
             Connection conexion = DriverManager.getConnection(
-                    "jdbc:mariadb://localhost:3306/tpo10_db4_herramientasConstruirSA_g3",
-                    "root",
-                    "");
+                "jdbc:mariadb://localhost:3306/tpo10_db4_herramientasConstruirSA_g3",
+                "root",
+                "");
+                     
+            // 3. Insertar 3 empleados.
             
-//            JOptionPane.showMessageDialog(null, "trabajador agregado");
-            
-            
-            //INSERT EMPLEADOS
-            
-//            String sql= "insert into empleado(dni, apellido, nombre, acceso, estado)"
-//                    + " values "
-//                    + "(42242442, 'hollmann', 'nicolas', 3, 1),"
-//                    + "(12436578, 'adam', 'rigg', 2,1),"
-//                    + "(98786354, 'enzo', 'casalegno',1,1);";
+            String sqlInsertEmpleados= "insert into empleado(dni, apellido, "
+                    + "nombre, acceso, estado)"
+                    + " values "
+                    + "(42242442, 'hollmann', 'nicolas', 3, 1),"
+                    + "(12436578, 'adam', 'rigg', 2,1),"
+                    + "(98786354, 'enzo', 'casalegno',1,1);";
 
-//            String sql = "insert into herramienta(nombre, descripcion, stock, estado)"
-//                    + "values"
-//                    + "('soldadora','MIG',5,1),"
-//                    + "('pala','ancha',15,1);";
+               // PreparedStatement PARA INSERT
 
-               //CONEXIÓN CON BD PARA INSERT
-
-//            PreparedStatement ps = conexion.prepareStatement(sql);
-//            int filas = ps.executeUpdate();
-//            if(filas > 0){
-//                JOptionPane.showMessageDialog(null, "Herramientas agregadas exitosamente");
-//                System.out.println(filas);
-//            }
+            PreparedStatement psInsertEmpleados = conexion.prepareStatement(
+                    sqlInsertEmpleados);
             
+            int filasEmpleados = psInsertEmpleados.executeUpdate();
+            if(filasEmpleados > 0){
+                JOptionPane.showMessageDialog(null,
+                        "Empleados agregadas exitosamente");
+                System.out.println(filasEmpleados);
+            }
 
+            // 4. Insertar 2 herramientas.
+
+            String sqlInsertHerramientas = "insert into herramienta(nombre, descripcion, stock, estado)"
+                    + "values"
+                    + "('soldadora','MIG',5,1),"
+                    + "('pala','ancha',15,1);";
+
+               // PreparedStatement PARA INSERT
+
+            PreparedStatement psInsertHerramientas = conexion.prepareStatement(
+                    sqlInsertHerramientas);
+            
+            int filasHerramientas = psInsertHerramientas.executeUpdate();
+            if(filasHerramientas > 0){
+                JOptionPane.showMessageDialog(null,
+                        "Herramientas agregadas exitosamente");
+                System.out.println(filasHerramientas);
+            }
+            
+            // 5. Listar todas las herramientas con stock superior a 10.
             //SELECT TABLA HERRAMIENTA
             
-//                String sql = "select * from herramienta where stock > 10";
-//
-//                //CONEXION DE BD PARA SELECT
-//            PreparedStatement ps = conexion.prepareStatement(sql);
-//            ResultSet cons = ps.executeQuery();
+                String sqlSelect = "select * from herramienta where stock > 10";
+
+                //CONEXION DE BD PARA SELECT
+            PreparedStatement psSelect = conexion.prepareStatement(sqlSelect);
+            ResultSet cons = psSelect.executeQuery();
 //            
 //            //MOSTRAR RESULTADOS
 //            
-//            while(cons.next()){
-//                System.out.println("ID: " + cons.getInt("idHerramienta"));
-//                System.out.println("Nombre: " + cons.getString("nombre"));
-//                System.out.println("Descripcion: " + cons.getString("descripcion"));
-//                System.out.println("Stock: " + cons.getInt("stock"));
-//                System.out.println("Estado: " + cons.getBoolean("estado"));
-//            }
+            while(cons.next()){
+                System.out.println("ID: " + cons.getInt("idHerramienta"));
+                System.out.println("Nombre: " + cons.getString("nombre"));
+                System.out.println("Descripcion: " + cons.getString("descripcion"));
+                System.out.println("Stock: " + cons.getInt("stock"));
+                System.out.println("Estado: " + cons.getBoolean("estado"));
+            }
 
-
+            // 6. Dar de baja al primer empleado ingresado a la base de datos.
                 //UPDATE AN EMPLEADO
                 
                String sql = "update empleado set estado = 0 where dni = 42242442";
@@ -77,12 +95,6 @@ public class Main {
                 JOptionPane.showMessageDialog(null, "Empleado dado de baja correctamente");             
             }
 
-                
-            
-            
-            
-            
-            
             //MANEJO DE ERRORES CONEXIONES
         } catch (SQLException ex) {
             ex.printStackTrace();
